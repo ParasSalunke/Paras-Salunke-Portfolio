2b4635f7cf6662c879ffb5ce65e7e37c358958fe
ParasSalunke-24 <salunkeparas24@gmail.com>
2024-09-08 15:36:40 +0530

    ✅ Added Type Effect in Hero Section Using npm typed.js

--- src/components/HeroSection/Hero.jsx
+++ src/components/HeroSection/Hero.jsx
@@ -1,3 +1,5 @@
+import { useEffect } from 'react';
+import Typed from 'typed.js';
 import './Hero.css';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
 import profile from '../../assets/images/Hero/hero_img_1.png';
@@ -9,6 +11,21 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const Hero = () => {
 
+    useEffect(() => {
+        const options = {
+            strings: ['React JS', 'Next JS', 'Node JS', 'Tailwind'],
+            typeSpeed: 50,
+            backSpeed: 50,
+            loop: true,
+        };
+
+        const typed = new Typed('#typed', options);
+
+        return () => {
+            typed.destroy();
+        };
+    }, []);
+
     const handleResumeClick = () => {
         toast.info('Resume not available now');
     };
@@ -16,7 +33,7 @@ const Hero = () => {
     return (
         <section id='home' className='hero'>
             <img src={profile} className='profile-img' alt='Paras Salunke' />
-            <h1><span>I&apos;m Paras Salunke,</span> a Full Stack Developer based in Pune.</h1>
+            <h1><span>I&apos;m Paras Salunke,</span> a Full Stack Developer based in Pune. <br /> I Code In <span id="typed"></span></h1>
             <p>I am a Full Stack Developer from Pune, Maharashtra. I specialize in creating beautiful, responsive, and fast websites. With 1 years of experience in web development, I have worked on various projects ranging from small business websites to large-scale web applications.</p>
             <div className="hero-action">
                 <AnchorLink className='anchor-link' offset={50} href="#contact">
